e4f8ced4f1926eddd10f69d140b04e6e78f9b234
tomas ignacio pereyra <tomaspereyra1997@gmail.com>
2021-07-12 20:29:19 -0300

    se agrego funcion EsPrimo

--- 00-primeros-pasos/funciones-flecha.js
+++ 00-primeros-pasos/funciones-flecha.js
@@ -4,5 +4,18 @@ const Sumar = (a, b) => a + b;
 
 const Multiplicar = (a, b) => a * b;
 
-const Dividir = (a, b) =>  a / b;
+const Dividir = (a, b) => a / b;
+
+const EsPrimo = numero => {
+
+    if (numero < 3)
+        return false;
+
+    for (let i = 2; i < numero; i++) {
+        if (numero % i === 0)
+            return false;
+    }
+
+    return true;
+}
 
